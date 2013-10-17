c7c8d5ad1ea3f08039e009ee8b10354ee7d4e5b3
Galen <galenjr@gmail.com>
2013-10-17 17:01:21 -0400

    remove github links

--- system/examples/views/index.php
+++ system/examples/views/index.php
@@ -3,7 +3,7 @@
 <ul>
 <?php foreach( $files as $file ): ?>
 	<?php if( strpos( basename( $file ), '_' ) !== 0 ): ?>
-	<li>- <a href="?example=<?php echo basename( $file ) ?>"><?php echo str_replace( '_', ' ', basename( $file, '.php' ) ) ?></a> &rarr; <a href="<?php echo GITHUB_URL ?><?php echo basename( $file ) ?>">github</a></li>
+	<li>- <a href="?example=<?php echo basename( $file ) ?>"><?php echo str_replace( '_', ' ', basename( $file, '.php' ) ) ?></a></li>
 	<?php endif; ?>
 <?php endforeach; ?>
 </ul>
